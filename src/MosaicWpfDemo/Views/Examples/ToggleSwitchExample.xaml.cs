2e83dabbec2235ac8685266da983ddbdf53ff0dd
Blake Pell <blakepell@hotmail.com>
2025-09-07 19:13:43 -0400

    Adds ToggleSwitch control example to demo app
    Adds a new example demonstrating the ToggleSwitch control to the demo application. This includes adding the ToggleSwitchExample view and updating the main window to include a navigation item for it. Also includes fixing an issue with the CornerRadius property not being applied to the ToggleSwitch control's thumb.

---
+++ src/MosaicWpfDemo/Views/Examples/ToggleSwitchExample.xaml.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Mosaic UI for WPF
+ *
+ * @project lead      : Blake Pell
+ * @website           : https://www.blakepell.com
+ * @website           : https://www.apexgate.net
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT - https://opensource.org/license/mit/
+ */
+
+using System.Windows;
+
+namespace MosaicWpfDemo.Views.Examples
+{
+    public partial class ToggleSwitchExample
+    {
+        public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register(
+            nameof(IsOn), typeof(bool), typeof(ToggleSwitchExample), new PropertyMetadata(default(bool)));
+
+        public bool IsOn
+        {
+            get => (bool)GetValue(IsOnProperty);
+            set => SetValue(IsOnProperty, value);
+        }
+
+        public ToggleSwitchExample()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+
+            this.IsOn = true;
+        }
+    }
+}
