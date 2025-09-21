3af3de6bdf3d02b3d4c2ad94dc0f389294bc6e15
Blake Pell <blakepell@hotmail.com>
2025-09-21 13:52:11 -0400

    ToggleButton Control

---
+++ src/Mosaic.UI.Wpf/Controls/ToggleButton/ToggleButton.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Mosaic UI for WPF
+ *
+ * @project lead      : Blake Pell
+ * @website           : https://www.blakepell.com
+ * @website           : https://www.apexgate.net
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT - https://opensource.org/license/mit/
+ */
+
+
+
+// ReSharper disable CheckNamespace
+
+namespace Mosaic.UI.Wpf.Controls
+{
+    /// <summary>
+    /// Represents a button control that can switch between two states: checked and unchecked.  This
+    /// implementation looks like a theme styled switch.
+    /// </summary>
+    public class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
+    {
+
+    }
+}
