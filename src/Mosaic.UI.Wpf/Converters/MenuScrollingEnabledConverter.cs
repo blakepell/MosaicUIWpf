87b5696373eba8ccad666fbeee171fb8c4312940
Blake Pell <blakepell@hotmail.com>
2025-09-22 14:58:59 -0400

    Menu Native Control Templates

---
+++ src/Mosaic.UI.Wpf/Converters/MenuScrollingEnabledConverter.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * Mosaic UI for WPF
+ *
+ * @project lead      : Blake Pell
+ * @website           : https://www.blakepell.com
+ * @website           : https://www.apexgate.net
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT - https://opensource.org/license/mit/
+ */
+
+using System.Globalization;
+using System.Windows.Data;
+using Mosaic.UI.Wpf.Common;
+
+namespace Mosaic.UI.Wpf.Converters
+{
+    /// <summary>
+    ///   Data binding converter to handle whether repeat buttons in scrolling
+    ///   menus are enabled.
+    /// </summary>
+    /// <remarks>
+    /// This is based on code from https://github.com/gix/PresentationTheme.Aero available via the MIT License
+    /// </remarks>
+    public sealed class MenuScrollingEnabledConverter : IMultiValueConverter
+    {
+        /// <summary>
+        ///   Converts a <see cref="System.Windows.Controls.ScrollViewer"/> state
+        ///   to a <see cref="bool"/> value indicating whether a specific scrolled
+        ///   percentage is reached.
+        /// </summary>
+        /// <param name="values">
+        ///   <see cref="double"/> values indicating the current state of the
+        ///   <see cref="System.Windows.Controls.ScrollViewer"/>.
+        ///   <list type="number">
+        ///     <item>
+        ///       <description>
+        ///         <see cref="System.Windows.Controls.ScrollViewer.VerticalOffset"/>
+        ///       </description>
+        ///     </item>
+        ///     <item>
+        ///       <description>
+        ///         <see cref="System.Windows.Controls.ScrollViewer.ExtentHeight"/>
+        ///       </description>
+        ///     </item>
+        ///     <item>
+        ///       <description>
+        ///         <see cref="System.Windows.Controls.ScrollViewer.ViewportHeight"/>
+        ///       </description>
+        ///     </item>
+        ///   </list>
+        /// </param>
+        /// <param name="targetType">
+        ///   The type of the binding target property.
+        /// </param>
+        /// <param name="parameter">
+        ///   A <see cref="double"/> value indicating the scrolled percentage at
+        ///   which the converter returns <see langword="true"/>. Use <c>0.0</c>
+        ///   or <c>100.0</c> to target the beginning or end respectively.
+        /// </param>
+        /// <param name="culture">
+        ///   The culture to use in the converter. <b>Not used.</b>
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the <paramref name="values"/> indicate a
+        ///   scrolled percentage that equals the target <paramref name="parameter"/>;
+        ///   otherwise <see langword="false"/>.
+        /// </returns>
+        public object Convert(object[]? values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var doubleType = typeof(double);
+            if (parameter == null ||
+                values == null ||
+                values.Length != 3 ||
+                values[0] == null ||
+                values[1] == null ||
+                values[2] == null ||
+                !doubleType.IsInstanceOfType(values[0]) ||
+                !doubleType.IsInstanceOfType(values[1]) ||
+                !doubleType.IsInstanceOfType(values[2]) ||
+                targetType != typeof(bool))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            var paramType = parameter.GetType();
+            if (!doubleType.IsAssignableFrom(paramType) &&
+                !typeof(string).IsAssignableFrom(paramType))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            // Conversion
+            double target;
+            if (parameter is string str)
+            {
+                target = double.Parse(str, NumberFormatInfo.InvariantInfo);
+            }
+            else
+            {
+                target = (double)parameter;
+            }
+
+            double verticalOffset = (double)values[0];
+            double extentHeight = (double)values[1];
+            double viewportHeight = (double)values[2];
+
+            if (extentHeight != viewportHeight)
+            {
+                // Calculate the percent so that we can see if we are near the edge of the range
+                double percent = Math.Min(100.0, Math.Max(0.0, (verticalOffset * 100.0 / (extentHeight - viewportHeight))));
+
+                if (DoubleUtil.AreClose(percent, target))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return [Binding.DoNothing];
+        }
+    }
+}
---
+++ src/Mosaic.UI.Wpf/Converters/TypeOfConverter.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Mosaic UI for WPF
+ *
+ * @project lead      : Blake Pell
+ * @website           : https://www.blakepell.com
+ * @website           : https://www.apexgate.net
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT - https://opensource.org/license/mit/
+ */
+
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mosaic.UI.Wpf.Converters
+{
+    /// <summary>
+    /// Converts a value to its <see cref="Type"/>.
+    /// </summary>
+    /// <remarks>
+    /// This is based on code from https://github.com/gix/PresentationTheme.Aero available via the MIT License
+    /// </remarks>
+    [ValueConversion(typeof(object), typeof(Type))]
+    public class TypeOfConverter : IValueConverter
+    {
+        /// <summary>Converts a value to its <see cref="Type"/>.</summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">
+        ///   The type of the binding target property.
+        /// </param>
+        /// <param name="parameter">
+        ///   The converter parameter to use. <b>Not used.</b>
+        /// </param>
+        /// <param name="culture">
+        ///   The culture to use in the converter. <b>Not used.</b>
+        /// </param>
+        /// <returns>
+        ///   The <see cref="Type"/> of <paramref name="value"/> or
+        ///   <see langword="null"/> if <paramref name="value"/> is
+        ///   <see langword="null"/>.
+        /// </returns>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value?.GetType();
+        }
+
+        object IValueConverter.ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
