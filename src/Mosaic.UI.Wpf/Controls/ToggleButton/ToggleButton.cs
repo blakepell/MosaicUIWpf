bf8f7351d8766bd156fba7d1fb1f06d659360758
Blake Pell <blakepell@hotmail.com>
2025-09-22 08:47:01 -0400

    ToggleButton updates for changing the background colors.

--- src/Mosaic.UI.Wpf/Controls/ToggleButton/ToggleButton.cs
+++ src/Mosaic.UI.Wpf/Controls/ToggleButton/ToggleButton.cs
@@ -8,8 +8,6 @@
  * @license           : MIT - https://opensource.org/license/mit/
  */
 
-
-
 // ReSharper disable CheckNamespace
 
 namespace Mosaic.UI.Wpf.Controls
@@ -23,6 +21,38 @@ namespace Mosaic.UI.Wpf.Controls
     /// </remarks>
     public class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
     {
+        /// <summary>
+        /// Identifies the <see cref="CheckedBackground"/> dependency property.
+        /// </summary>
+        /// <remarks>This property represents the background brush used when the <see
+        /// cref="ToggleButton"/> is in the checked state.</remarks>
+        public static readonly DependencyProperty CheckedBackgroundProperty =
+            DependencyProperty.Register(nameof(CheckedBackground), typeof(Brush), typeof(ToggleButton), new PropertyMetadata(Brushes.Transparent));
 
+        /// <summary>
+        /// Gets of sets the background color when the ToggleButton is in the "Checked" state.
+        /// </summary>
+        public Brush CheckedBackground
+        {
+            get => (Brush)GetValue(CheckedBackgroundProperty);
+            set => SetValue(CheckedBackgroundProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="UncheckedBackground"/> dependency property.
+        /// </summary>
+        /// <remarks>This property represents the background brush used for the <see cref="ToggleButton"/>
+        /// when it is in the unchecked state. The default value is <see cref="Brushes.Transparent"/>.</remarks>
+        public static readonly DependencyProperty UncheckedBackgroundProperty =
+            DependencyProperty.Register(nameof(UncheckedBackground), typeof(Brush), typeof(ToggleButton), new PropertyMetadata(Brushes.Transparent));
+
+        /// <summary>
+        /// Gets or sets the background color when the ToggleButton is in the "Unchecked" state.
+        /// </summary>
+        public Brush UncheckedBackground
+        {
+            get => (Brush)GetValue(UncheckedBackgroundProperty);
+            set => SetValue(UncheckedBackgroundProperty, value);
+        }
     }
 }
