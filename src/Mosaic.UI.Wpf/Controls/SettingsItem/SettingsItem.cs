2f7bb2d8595ab124418a538fce8d2f17dd2f0deb
Blake Pell <blakepell@hotmail.com>
2025-09-24 16:36:44 -0400

    SettingsItem Control

---
+++ src/Mosaic.UI.Wpf/Controls/SettingsItem/SettingsItem.cs
@@ -0,0 +1,57 @@
+﻿namespace Mosaic.UI.Wpf.Controls
+{
+    public class SettingsItem : ContentControl
+    {
+        static SettingsItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(SettingsItem),
+                new FrameworkPropertyMetadata(typeof(SettingsItem)));
+        }
+
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.Register(nameof(IconSource), typeof(ImageSource), typeof(SettingsItem));
+
+        public ImageSource IconSource
+        {
+            get => (ImageSource)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(SettingsItem));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(nameof(Description), typeof(string), typeof(SettingsItem));
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(SettingsItem), new PropertyMetadata(new CornerRadius(4)));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.Register(nameof(IconSize), typeof(double), typeof(SettingsItem), new PropertyMetadata(24.0));
+
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+    }
+}
