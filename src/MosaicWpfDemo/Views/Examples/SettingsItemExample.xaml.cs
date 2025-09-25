47f8a4ef63f1ae5f370407c6a4fd7616898a7ea4
Blake Pell <blakepell@hotmail.com>
2025-09-25 12:09:59 -0400

    Adds a settings item example to the demo

---
+++ src/MosaicWpfDemo/Views/Examples/SettingsItemExample.xaml.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Mosaic UI for WPF
+ *
+ * @project lead      : Blake Pell
+ * @website           : https://www.blakepell.com
+ * @website           : https://www.apexgate.net
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT - https://opensource.org/license/mit/
+ */
+
+using System.Windows;
+
+namespace MosaicWpfDemo.Views.Examples
+{
+    public partial class SettingsItemExample
+    {
+        public static readonly DependencyProperty TestSettingProperty = DependencyProperty.Register(
+            nameof(TestSetting), typeof(bool), typeof(SettingsItemExample), new PropertyMetadata(false));
+
+        public bool TestSetting
+        {
+            get => (bool)GetValue(TestSettingProperty);
+            set => SetValue(TestSettingProperty, value);
+        }
+
+        public static readonly DependencyProperty DefaultUsernameProperty = DependencyProperty.Register(
+            nameof(DefaultUsername), typeof(string), typeof(SettingsItemExample), new PropertyMetadata(Environment.UserName));
+
+        public string DefaultUsername
+        {
+            get => (string)GetValue(DefaultUsernameProperty);
+            set => SetValue(DefaultUsernameProperty, value);
+        }
+
+        public SettingsItemExample()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
