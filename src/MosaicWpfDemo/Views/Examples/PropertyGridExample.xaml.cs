78d2a59f4defcede823a376d91b475561ff0dad5
Blake Pell <bpell@iu.edu>
2025-10-01 13:54:42 -0400

    Moved model

--- src/MosaicWpfDemo/Views/Examples/PropertyGridExample.xaml.cs
+++ src/MosaicWpfDemo/Views/Examples/PropertyGridExample.xaml.cs
@@ -8,8 +8,11 @@
  * @license           : MIT - https://opensource.org/license/mit/
  */
 
+using CommunityToolkit.Mvvm.ComponentModel;
 using Mosaic.UI.Wpf.Controls;
+using System.Collections.ObjectModel;
 using System.Windows.Media;
+using System.ComponentModel;
 
 namespace MosaicWpfDemo.Views.Examples
 {
@@ -29,5 +32,71 @@ namespace MosaicWpfDemo.Views.Examples
                 Color = Colors.Blue
             };
         }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Unspecified
+    }
+
+    public partial class Person : ObservableObject
+    {
+        [property: PropertyGrid(MaxLength = 12)]
+        [property: DisplayName("Username")]
+        [property: Category("Account")]
+        [property: Description("The users account name.")]
+        [ObservableProperty]
+        private string? _username;
+
+        [property: DisplayName("Full Name")]
+        [property: Category("Identity")]
+        [property: Description("The person's full name")]
+        [ObservableProperty]
+        private string _name;
+
+        [property: Category("Identity")]
+        [property: Description("The person's age")]
+        [ObservableProperty]
+        private int _age;
+
+        [property: Category("Identity")]
+        [ObservableProperty]
+        private DateOnly _birthday;
+
+        [property: Category("Account")]
+        [ObservableProperty]
+        private bool _active;
+
+        [property: PropertyGrid(IsReadOnly = true, Description = "This field is read only.")]
+        [property: Category("Account")]
+        [ObservableProperty]
+        private bool _disabledBool;
+
+        [property: Category("Giving")]
+        [ObservableProperty]
+        private double _lifetimeGiving = 0.00;
+
+        [property: PropertyGrid(IsReadOnly = true)]
+        [ObservableProperty]
+        private string _notes = "This is a readonly field.";
+
+        [property: Category("Account")]
+        [ObservableProperty]
+        private DateTime? _lastActive;
+
+        [property: Category("Appearance")]
+        [ObservableProperty]
+        private Color _color;
+
+        [property: Category("Identity")]
+        [ObservableProperty]
+        private Gender _gender;
+
+        [property: PropertyGrid(Ignore = true)]
+        [property: Category("Identity")]
+        [ObservableProperty]
+        private ObservableCollection<string> _nickNames = new();
     }
 }
