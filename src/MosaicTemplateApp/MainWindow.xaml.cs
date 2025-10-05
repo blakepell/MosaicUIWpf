339adda8a626d4bd89ef49e53820bc87d85df4fe
Blake Pell <blakepell@hotmail.com>
2025-10-05 14:37:55 -0400

    Removed unused usings.

--- src/MosaicTemplateApp/MainWindow.xaml.cs
+++ src/MosaicTemplateApp/MainWindow.xaml.cs
@@ -8,20 +8,10 @@
  * @license           : MIT - https://opensource.org/license/mit/
  */
 
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Argus.Memory;
-using Mosaic.UI.Wpf.Common;
 using Mosaic.UI.Wpf.Themes;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace MosaicTemplateApp
 {
