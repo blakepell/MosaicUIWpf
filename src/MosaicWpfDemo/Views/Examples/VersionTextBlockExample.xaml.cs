dc114620f163b7bc80f4c30cb022f5d1cb4a43f2
Blake Pell <bpell@iu.edu>
2025-10-20 16:48:54 -0400

    Shield version example

---
+++ src/MosaicWpfDemo/Views/Examples/VersionTextBlockExample.xaml.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Mosaic UI for WPF
+ *
+ * @project lead      : Blake Pell
+ * @website           : https://www.blakepell.com
+ * @website           : https://www.apexgate.net
+ * @copyright         : Copyright (c), 2023-2025 All rights reserved.
+ * @license           : MIT - https://opensource.org/license/mit/
+ */
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace MosaicWpfDemo.Views.Examples
+{
+    public partial class VersionTextBlockExample
+    {
+        public VersionTextBlockExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
