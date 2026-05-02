66d59d8b1d0ffc9d06503b6c567a3a4095bc184b
Blake Pell <blakepell@hotmail.com>
2026-05-02 15:28:45 -0400

    UI Updates

--- src/LanChat/MainWindow.xaml.cs
+++ src/LanChat/MainWindow.xaml.cs
@@ -9,7 +9,6 @@
  */
 
 using System.Windows;
-using System.Windows.Input;
 using Argus.Memory;
 using LanChat.Common;
 using LanChat.Network;
@@ -23,7 +22,6 @@ namespace LanChat
         public MainWindow()
         {
             InitializeComponent();
-            UpdateMaximizeIcon();
             this.DataContext = AppServices.GetRequiredService<AppViewModel>();
         }
 
@@ -57,63 +55,6 @@ namespace LanChat
             }
 
             vm.LanIpAddress = "127.0.0.1";
-        }
-
-        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                ToggleMaximize();
-            }
-            else
-            {
-                DragMove();
-            }
-        }
-
-        private void Minimize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void MaximizeRestore_Click(object sender, RoutedEventArgs e)
-        {
-            ToggleMaximize();
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void ToggleMaximize()
-        {
-            if (WindowState == WindowState.Maximized)
-            {
-                WindowState = WindowState.Normal;
-            }
-            else
-            {
-                WindowState = WindowState.Maximized;
-            }
-
-            UpdateMaximizeIcon();
-        }
-
-        private void UpdateMaximizeIcon()
-        {
-            if (MaxRestoreButton == null)
-            {
-                return;
-            }
-
-            MaxRestoreButton.Content = WindowState == WindowState.Maximized ? "\xE923" : "\xE922";
-        }
-
-        protected override void OnStateChanged(EventArgs e)
-        {
-            base.OnStateChanged(e);
-            UpdateMaximizeIcon();
         }
 
         private void ButtonToggleTheme_OnClick(object sender, RoutedEventArgs e)
