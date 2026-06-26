4afe936636ceeab3adfb346bc9c5a58602c4b072
Blake Pell <blakepell@hotmail.com>
2026-06-26 12:50:02 -0400

    ChatThread Attribution

--- src/Mosaic.UI.Wpf/Controls/Chat/ChatThread.xaml.cs
+++ src/Mosaic.UI.Wpf/Controls/Chat/ChatThread.xaml.cs
@@ -1,11 +1,7 @@
 ﻿/*
  * Mosaic UI for WPF
  *
- * @project lead      : Blake Pell
- * @website           : https://www.blakepell.com
- * @website           : https://www.apexgate.net
- * @copyright         : Copyright (c), 2023-2026 All rights reserved.
- * @license           : MIT - https://opensource.org/license/mit/
+ * ChatThread Based on: https://github.com/SamKr/ChatBubbles
  */
 
 using System.Collections.ObjectModel;
