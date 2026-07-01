cc2b2fa3f4cdd85525f839d82ca5a8b83a4119b6
Blake Pell <blakepell@hotmail.com>
2026-07-01 14:45:34 -0400

    Additional native control updates

--- src/MosaicWpfDemo/Views/Examples/NativeControlsExample.xaml.cs
+++ src/MosaicWpfDemo/Views/Examples/NativeControlsExample.xaml.cs
@@ -8,6 +8,9 @@
  * @license           : MIT - https://opensource.org/license/mit/
  */
 
+using System.Diagnostics;
+using System.Windows.Navigation;
+
 namespace MosaicWpfDemo.Views.Examples
 {
     public partial class NativeControlsExample
@@ -15,6 +18,12 @@ namespace MosaicWpfDemo.Views.Examples
         public NativeControlsExample()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
