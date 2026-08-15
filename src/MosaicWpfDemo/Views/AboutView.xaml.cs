afa1ccae40a846ae3ec0f5a7513a4694745b74d8
Blake Pell <blakepell@hotmail.com>
2026-08-15 10:31:09 -0400

    About View and Theme Settings page split out.

--- src/MosaicWpfDemo/Views/AboutView.xaml.cs
+++ src/MosaicWpfDemo/Views/AboutView.xaml.cs
@@ -8,128 +8,13 @@
  * @license           : MIT - https://opensource.org/license/mit/
  */
 
-using System.Windows;
-using System.Windows.Controls;
-using Argus.Memory;
-using Mosaic.UI.Wpf;
-using Mosaic.UI.Wpf.Themes;
-using MosaicWpfDemo.Common;
-using MosaicMosaicThemeMode = Mosaic.UI.Wpf.MosaicThemeMode;
-
 namespace MosaicWpfDemo.Views
 {
     public partial class AboutView
     {
-        private bool _syncingThemeControls;
-
         public AboutView()
         {
             InitializeComponent();
-        }
-
-        private void AboutView_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            SyncThemeEditorFromManager();
-        }
-
-        private void MosaicThemeModeSelector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (_syncingThemeControls)
-            {
-                return;
-            }
-
-            if (MosaicThemeModeSelector.SelectedItem is not ComboBoxItem selected || selected.Tag is not string selectedTheme)
-            {
-                return;
-            }
-
-            if (!Enum.TryParse<MosaicMosaicThemeMode>(selectedTheme, ignoreCase: true, out var mode))
-            {
-                return;
-            }
-
-            var manager = AppServices.GetService<ThemeManager>();
-            if (manager == null)
-            {
-                return;
-            }
-
-            manager.Theme = mode;
-            PersistTheme(mode);
-        }
-
-        private void NativeStylesCheckBox_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (_syncingThemeControls)
-            {
-                return;
-            }
-
-            var manager = AppServices.GetService<ThemeManager>();
-            if (manager == null)
-            {
-                return;
-            }
-
-            manager.Native = NativeStylesCheckBox.IsChecked == true;
-        }
-
-        private void SystemColorsCheckBox_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (_syncingThemeControls)
-            {
-                return;
-            }
-
-            var manager = AppServices.GetService<ThemeManager>();
-            if (manager == null)
-            {
-                return;
-            }
-
-            manager.SystemColors = SystemColorsCheckBox.IsChecked == true;
-        }
-
-        private void SyncThemeEditorFromManager()
-        {
-            var manager = AppServices.GetService<ThemeManager>();
-            if (manager == null)
-            {
-                return;
-            }
-
-            _syncingThemeControls = true;
-
-            NativeStylesCheckBox.IsChecked = manager.Native;
-            SystemColorsCheckBox.IsChecked = manager.SystemColors;
-
-            ComboBoxItem? selectedItem = null;
-
-            foreach (var item in MosaicThemeModeSelector.Items)
-            {
-                if (item is ComboBoxItem comboBoxItem &&
-                    string.Equals(comboBoxItem.Tag as string, manager.Theme.ToString(), StringComparison.OrdinalIgnoreCase))
-                {
-                    selectedItem = comboBoxItem;
-                    break;
-                }
-            }
-
-            MosaicThemeModeSelector.SelectedItem = selectedItem;
-
-            _syncingThemeControls = false;
-        }
-
-        private static void PersistTheme(MosaicMosaicThemeMode theme)
-        {
-            if (!AppServices.IsRegistered<AppSettings>())
-            {
-                return;
-            }
-
-            var appSettings = AppServices.GetRequiredService<AppSettings>();
-            appSettings.Theme = theme;
         }
     }
 }
